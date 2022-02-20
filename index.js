d393a96a6c8fe7444bfaaae3a6fb4afb9999640f
CosmicRacing <91793115+CosmicRacing@users.noreply.github.com>
2022-02-20 11:29:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -77,3 +77,23 @@ window.onresize = function(event){
         clearInterval(automaticChange);
     }
 }
+
+
+let touchstartX = 0
+let touchendX = 0
+
+const memberCard = document.getElementById('member-container')
+
+function handleGesture() {
+  if (touchendX < touchstartX) alert('swiped left!')
+  if (touchendX > touchstartX) alert('swiped right!')
+}
+
+slider.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+slider.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  handleGesture()
+})
